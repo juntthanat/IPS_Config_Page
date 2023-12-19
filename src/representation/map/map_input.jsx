4b58b3d742f1565345ad0b7f919efe0736c52ba4
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-12-19 13:59:29 +0700

    feat: add image

--- src/representation/map/map_input.jsx
+++ src/representation/map/map_input.jsx
@@ -1,10 +1,11 @@
 import "./map_input.css"
+import map1 from "../../assets/map_1.png"
 
 export default function MapInput(){
     return (
         <div id="map-input-background">
             <div id="map-input-container">
-                MAP
+                <img src={map1} alt="Map" draggable="false"></img>
             </div>
         </div>
     )
