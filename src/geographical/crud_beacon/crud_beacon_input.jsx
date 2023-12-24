46572c3fcef68e6a63f552c9b8011be8a5d669c6
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2023-12-24 23:34:36 +0700

    feat: prettier

--- src/geographical/crud_beacon/crud_beacon_input.jsx
+++ src/geographical/crud_beacon/crud_beacon_input.jsx
@@ -7,18 +7,21 @@ import CRUDBeaconGeoX from "./crud_beacon_geo_x";
 import CRUDBeaconGeoY from "./crud_beacon_geo_y";
 import CRUDBeaconGeoFloorId from "./crud_beacon_geo_floor_id";
 
-export default function CRUDBeaconInput(){
-    return (
-        <div id="crud-beacon-input" style={{display: "flex", flexDirection: "column"}}>
-            {TopicHeader("CRUD Beacon")}
-            {CRUDBeaconId()}
-            {CRUDBeaconMacAddress()}
-            {CRUDBeaconName()}
-            {CRUDBeaconGeoX()}
-            {CRUDBeaconGeoY()}
-            {CRUDBeaconGeoFloorId()}
-        </div>  
-    )
+export default function CRUDBeaconInput() {
+  return (
+    <div
+      id="crud-beacon-input"
+      style={{ display: "flex", flexDirection: "column" }}
+    >
+      {TopicHeader("CRUD Beacon")}
+      {CRUDBeaconId()}
+      {CRUDBeaconMacAddress()}
+      {CRUDBeaconName()}
+      {CRUDBeaconGeoX()}
+      {CRUDBeaconGeoY()}
+      {CRUDBeaconGeoFloorId()}
+    </div>
+  );
 }
 
 /*
@@ -29,4 +32,20 @@ CRUD beacon
 - geo_x
 - geo_y
 - floor_id
+*/
+
+/*
+fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    body: JSON.stringify({
+        userId: 1,
+        title: "Fix my bugs",
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
 */
--- src/representation/map/map_input.jsx
+++ src/representation/map/map_input.jsx
@@ -1,12 +1,12 @@
-import "./map_input.css"
-import map1 from "../../assets/map_1.png"
+import "./map_input.css";
+import map1 from "../../assets/map_1.png";
 
-export default function MapInput(){
-    return (
-        <div id="map-input-background">
-            <div id="map-input-container">
-                <img src={map1} alt="Map" draggable="false"></img>
-            </div>
-        </div>
-    )
+export default function MapInput() {
+  return (
+    <div id="map-input-background">
+      <div id="map-input-container">
+        <img src={map1} alt="Map" draggable="false"></img>
+      </div>
+    </div>
+  );
 }
