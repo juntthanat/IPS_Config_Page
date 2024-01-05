004e6725a9837345e627eb93857b9df779816c27
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-01-05 20:04:25 +0700

    feat: button test

--- src/confirm-cancel-button/confirm_cancel_button.jsx
+++ src/confirm-cancel-button/confirm_cancel_button.jsx
@@ -1,10 +1,22 @@
-import "./confirm_cancel_button.css"
+import "./confirm_cancel_button.css";
 
 export default function ConfirmCancelButton() {
+  const confirmClickHandler = () => {
+    console.log("Confirm Was Clicked");
+  };
+
+  const cancelClickHandler = () => {
+    console.log("Cancel Was Clicked");
+  };
+
   return (
     <div id="confirm-cancel-container">
-      <button id="cancel-button">Cacncel</button>
-      <button id="confirm-button">Confirm</button>
+      <button id="cancel-button" onClick={cancelClickHandler}>
+        Cacncel
+      </button>
+      <button id="confirm-button" onClick={confirmClickHandler}>
+        Confirm
+      </button>
     </div>
   );
 }
