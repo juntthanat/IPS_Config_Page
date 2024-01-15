d04f45f7394d095d504a3e26f4fb905658658e95
VirtuousCrane <ray.daroonpat.2020@gmail.com>
2024-01-16 01:57:08 +0700

    Changed map_input from using img to using canvas

--- src/map/map_input.jsx
+++ src/map/map_input.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useRef } from "react";
 import "./map_input.css";
 import map1 from "../assets/map_1.png";
 
 import ConfirmCancelButton from "../confirm-cancel-button/confirm_cancel_button";
 
 export default function MapInput() {
+  
+  const canvasRef = useRef(null);
+
+  // useEffect with [] as param to execute only at mount time
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const parentBoundingBox = canvas.parentNode.getBoundingClientRect();
+    canvas.width = parentBoundingBox.width;
+    canvas.height = parentBoundingBox.height;
+
+    const image = new Image();
+    image.src = map1;
+    image.onload = () => {
+	const imageAspectRatio = image.height / image.width;
+	const imageHeight = canvas.height * 0.84;
+	const imageWidth = imageHeight / imageAspectRatio;
+
+	const imageX = (canvas.width - imageWidth) / 2;
+	const imageY = (canvas.height - imageHeight) / 2;
+	context.drawImage(image, imageX, imageY, imageWidth, imageHeight);
+    };
+  }, []);
+
+  // <img src={map1} alt="Map" draggable="false"></img>
   return (
     <div id="map-input-background">
       <div id="map-input-container">
-        <img src={map1} alt="Map" draggable="false"></img>
+	<canvas ref={canvasRef}></canvas>
       </div>
       {ConfirmCancelButton()}
     </div>
