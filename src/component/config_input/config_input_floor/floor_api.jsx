fa0bdeee8e4ac26a154e94d77a3c76ec94583156
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-02-06 16:53:15 +0700

    feat: rename base URL

--- src/component/config_input/config_input_floor/floor_api.jsx
+++ src/component/config_input/config_input_floor/floor_api.jsx
@@ -1,23 +1,15 @@
 import { useState, useEffect } from "react";
 
 export default function FloorAPI() {
-  const baseURL = `http://marco.cooldev.win:8080/`;
+  const baseURL = `http://marco.cooldev.win:8080/api/v1`;
   const [data, setData] = useState([]);
 
   const fetchInfo = () => {
-    return (
-      fetch(baseURL + `api/v1/floors`, {
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      })
-        //   .then((e) => e.json())
-        .then((d) => setData(d))
-    );
+    return fetch(baseURL + `/floors`, {
+      mode: "no-cors",
+    })
+      .then((e) => e.json())
+      .then((d) => setData(d));
   };
 
   useEffect(() => {
