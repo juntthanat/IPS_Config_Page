a531a1cffce414135e5e5e7d426fe2d73bddea17
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-03-17 08:48:29 +0700

    feat: finish delete function

---
+++ src/modal-page/beacon/delete_beacon_information.jsx
@@ -0,0 +1,22 @@
+export default function DeleteBeaconInformation(selectedBeacon) {
+  const baseURL = `http://marco.cooldev.win:8080/api/v1`;
+  const requestOptions = {
+    method: "DELETE",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  };
+
+  const deleteInfo = async () => {
+    return await fetch(baseURL + `/beacons/` + selectedBeacon, requestOptions)
+        .then((res) => res.json())
+        .then((res) => JSON.parse(JSON.stringify(res)))
+        .catch((error) => console.log(error))
+  };
+
+  deleteInfo();
+
+  return <div></div>
+}
