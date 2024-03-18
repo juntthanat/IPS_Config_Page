a90958577a9538e8d556fc7fd9293271316e71a6
Chayut Liewlom <ray.daroonpat.2020@gmail.com>
2024-03-18 15:36:02 +0700

    Update location_api.jsx

--- src/component/config_input/config_input_location/location_api.jsx
+++ src/component/config_input/config_input_location/location_api.jsx
@@ -39,7 +39,7 @@ export default function LocationAPI(props) {
 
   useEffect(() => {
     fetchLocationId();
-  }, [selectedFloor, rerender]);
+  }, [selectedFloor]);
 
   useEffect(() => {
     locationIdList(data);
