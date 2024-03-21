d87ba2df26b070f9d7cc5d6709eadbb0fbebc59f
Janthanat Thanintantrakun <73385307+juntthanat@users.noreply.github.com>
2024-03-21 13:13:44 +0700

    feat: fixed Bug
    beacon name now doesn't dup after editing the beacon information

--- src/component/config_input/config_input_beacon/beacon_api.jsx
+++ src/component/config_input/config_input_beacon/beacon_api.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useRef } from "react";
 import { RerenderContext } from "../../../App";
 
 export default function BeaconAPI(props) {
-  const { selectedBeacon, setSelectedBeacon, selectedFloor } =
-    props ?? {};
+  const { selectedBeacon, setSelectedBeacon, selectedFloor } = props ?? {};
   const baseURL = `http://marco.cooldev.win:8080/api/v1`;
   const [data, setData] = useState([]);
   const [beaconList, setBeaconList] = useState([]);
   const [beaconName, setBeaconName] = useState([]);
+  const isFetching = useRef(false);
 
   const { rerenderValuePlaceholder } = useContext(RerenderContext);
 
@@ -28,17 +28,21 @@ export default function BeaconAPI(props) {
   };
 
   const fetchBeaconName = async (idList) => {
-    setBeaconName([]);
-    for (let i = 0; i < idList.length; i++) {
-      await fetch(baseURL + `/beacons/` + idList[i])
-        .then((e) => e.json())
-        .then((d) => JSON.parse(JSON.stringify(d)))
-        .then((f) => setBeaconName((beaconName) => [...beaconName, f]));
+    if (isFetching.current == false) {
+      isFetching.current = true;
+      setBeaconName([]);
+      for (let i = 0; i < idList.length; i++) {
+        await fetch(baseURL + `/beacons/` + idList[i])
+          .then((e) => e.json())
+          .then((d) => JSON.parse(JSON.stringify(d)))
+          .then((f) => setBeaconName((beaconName) => [...beaconName, f]));
+      }
     }
+    isFetching.current = false;
   };
 
   useEffect(() => {
-      fetchBeaconId();
+    fetchBeaconId();
   }, [selectedFloor, rerenderValuePlaceholder]);
 
   useEffect(() => {
